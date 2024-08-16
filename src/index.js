678f016e9e9099247349647a25655219e9d5f9cf
Bidji <159267099+weBidji@users.noreply.github.com>
2024-08-16 12:21:37 -0700

    basic information fetch and display

--- src/index.js
+++ src/index.js
@@ -1,23 +1,54 @@
 import "./style.css";
-let temperature;
-let description;
-let currentLocation;
+import { format, parse } from "date-fns";
 
-async function getTemp(location) {
-  currentLocation = location;
+let locationData;
+let currentDate;
+let localTime;
+
+let temperature;
+let forecast;
+
+let humidity;
+let windSpeed;
+let feelsLike;
+
+// fetch weather data
+async function getData(location) {
   try {
     const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=7FNLWYQA3L2SBVYWBT882KRJV&contentType=json`
+      `http://api.weatherapi.com/v1/forecast.json?key=63de79d20d6e4501af8183353241608&q=${location}&days=5&aqi=no&alerts=no`
     );
     const data = await response.json();
     console.log(data);
-    description = data.description;
-    temperature = data.currentConditions.temp;
-    console.log(`Current temperature in ${location}: ${temperature}°C.`);
-    console.log(description);
+
+    // location
+
+    locationData = `${data.location.name}, ${data.location.region}, ${data.location.country}`;
+    // date and time
+    const timeInfo = data.location.localtime.split(" ");
+    const date = parse(timeInfo[0], "yyyy-MM-dd", new Date());
+    currentDate = format(date, "EEEE, MMMM do yyyy");
+    localTime = timeInfo[1];
+    // forecast
+
+    forecast = data.current.condition.text;
+    console.log(forecast);
+
+    // temp
+    temperature = data.current.temp_c.toFixed(0);
+    console.log(temperature);
+
+    /* RIght side */
+
+    //Humidity, wind, feels like
+
+    humidity = data.current.humidity;
+    windSpeed = data.current.wind_kph;
+    feelsLike = data.current.feelslike_c.toFixed(0);
+
     displayInfo();
   } catch (err) {
-    console.log("location fetching failed");
+    console.log(err);
   }
 }
 
@@ -26,7 +57,8 @@ const searchBtn = document.getElementById("search-btn");
 
 searchBtn.addEventListener("click", () => {
   if (locationInput.value) {
-    getTemp(locationInput.value.trim().toLowerCase());
+    const trimmedLocation = locationInput.value.trim().toLowerCase();
+    getData(trimmedLocation);
     locationInput.value = "";
   } else {
     console.log("please enter a location to search");
@@ -34,9 +66,38 @@ searchBtn.addEventListener("click", () => {
 });
 
 function displayInfo() {
+  const locationDisplay = document.getElementById("location");
+  const dateInfo = document.getElementById("date-info");
+  const timeInfo = document.getElementById("time-info");
   const tempBox = document.getElementById("temperature");
-  const forecast = document.getElementById("forecast");
+  const forecastInfo = document.getElementById("forecast-info");
+  const humidityInfo = document.getElementById("humidity-info");
+  const windInfo = document.getElementById("wind-info");
+  const feelsLikeInfo = document.getElementById("feels-like-info");
 
-  tempBox.textContent = `Current temperature in ${currentLocation}: ${temperature}°C`;
-  forecast.textContent = `${description}`;
+  // left side
+  locationDisplay.textContent = `${locationData}`;
+  dateInfo.textContent = `${currentDate}`;
+  timeInfo.textContent = `${localTime}`;
+  tempBox.textContent = `${temperature}°C`;
+  forecastInfo.textContent = `${forecast}`;
+  // right side
+
+  humidityInfo.textContent = `Humidity: ${humidity}%`;
+  windInfo.textContent = `Wind: ${windSpeed} km/h`;
+  feelsLikeInfo.textContent = `Feels like: ${feelsLike}°C`;
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+  getData("paris");
+});
+
+// refactor for good practice
+
+// refine current info
+
+// add 3-5 days forecast
+
+// add imperial / metric converter
+
+// display style
